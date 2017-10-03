03026f78d6d7fe613f3f8389e02996a59c0865ca
Adam Morley <adam@adammorley.co.uk>
2017-10-03 15:38:44 +0100

    Reworked SVG anim for safari and iphone

--- dev/script.js
+++ dev/script.js
@@ -1,15 +1,15 @@
 function init() {
   console.log('start animation');
 
+
+
   // store button and animation ids in vars
   var muteButton = document.querySelector('.mute-button');
   var playButton = document.querySelector('.play-button');
-  var playToPauseOne = document.querySelector('.play-to-pause-one');
-  var pauseToPlayOne = document.querySelector('.pause-to-play-one');
-  var playToPauseTwo = document.querySelector('.play-to-pause-two');
-  var pauseToPlayTwo = document.querySelector('.pause-to-play-two');
+  var playButtonLeft = document.querySelector('.play-button-left');
+  var playButtonRight = document.querySelector('.play-button-right');
   // store state of button in var
-  var playing = true;
+  var playing = false;
 
   // create on click listeners
   muteButton.addEventListener("click", toggleMute);
@@ -21,18 +21,40 @@ function init() {
 
   }
 
-  // toggle the state of the play button and tell relevant svg animation to begin
+
+var pauseShapeOne = "92 151 67 151 67 64 92 64 92 108";
+var playShapeOne = "142 108 67 151 67 64 142 108 142 108";
+var pauseShapeTwo = "142 151 117 151 117 64 142 64 142 108";
+var playShapeTwo = "142 108 67 151 67 64 142 108 142 108";
+
+
+  // toggle the state of the play button 
   function togglePlay() {
-  	if (playing === true) {
-  		playToPauseOne.beginElement();  		
-  		playToPauseTwo.beginElement();
-  		playing = false;
-  	} else {
-  		pauseToPlayOne.beginElement();
-  		pauseToPlayTwo.beginElement();
+    // If the video is paused show the play button
+  	if (playing === false) {
+      //change the from and to attributes of the svg animate tag
+      // add the new coordinates for the shape from the vars
+  		playButtonLeft.setAttribute("from", playShapeOne);
+  		playButtonLeft.setAttribute("to", pauseShapeOne);
+  		playButtonRight.setAttribute("from", playShapeTwo);
+  		playButtonRight.setAttribute("to", pauseShapeTwo);
+      playButtonLeft.beginElement(); 
+      playButtonRight.beginElement(); 
   		playing = true;
+  	} else {
+      // do the opposite
+  		playButtonLeft.setAttribute("from", pauseShapeOne);
+  		playButtonLeft.setAttribute("to", playShapeOne);
+  		playButtonRight.setAttribute("from", pauseShapeTwo);
+  		playButtonRight.setAttribute("to", playShapeTwo);
+      playButtonLeft.beginElement(); 
+      playButtonRight.beginElement(); 
+  		playing = false;
   	}
 
   }
+
+
+
 
 }
